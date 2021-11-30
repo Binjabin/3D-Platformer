a1e437593d7fd114d3bff88be8bdb9c050ce7bbc
Ben Smith <bsmith2021@elam.co.uk>
2021-11-30 16:00:26 +0000

    Added crystal partical's and pickupability. Updated to 2021 version.

---
+++ Assets/Scripts/Crystal.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crystal : MonoBehaviour
+{
+    [SerializeField] GameObject visuals;
+    [SerializeField] ParticleSystem particles;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "player")
+        {
+            visuals.SetActive(false);
+            GetComponent<Collider>().enabled = false;
+            particles.Play();
+
+        }
+    }
+
+}
